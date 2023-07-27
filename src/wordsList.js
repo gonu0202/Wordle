6202dc0340c32cc2d4f0f28905627367028d4f7f
aniket-fareye <aniket.kumar@getfareye.com>
2023-07-27 20:02:03 +0530

    enhanced words library

--- src/wordsList.js
+++ src/wordsList.js
@@ -12,7 +12,17 @@ const fiveLetterWords = [
     "Water", "Apple", "Tiger", "House", "Rabbit", "Beach", "Mouse", "Laugh", "Sword", "Horse",
     "Angel", "Earth", "Heart", "Clock", "Snake", "Fairy", "Storm", "Wings", "Ghost", "Night",
     "Mouse", "Happy", "Light", "Stars", "Chess", "Brain", "Shoes", "Skirt", "World", "Fruit",
-    "Plant", "Cloud", "Tiger", "Dance"
+    "Plant", "Cloud", "Tiger", "Dance","Apple", "Mango", "Bread", "Grape", "Peach", "Flour", 
+    "Juice", "Honey", "Olive", "Coral",
+    "Tiger", "Eagle", "Mouse", "Whale", "Panda", "Sheep", "Horse", "Snake", "Zebra", "Bunny",
+    "Tulip", "Lily", "Daisy", "Cedar", "Maple", "Birch", "Cedar", "Cactus", "Melon", "Carrot",
+    "Onion", "Chili", "Basil", "Mint", "Olive", "Lemon", "Peach", "Plum", "Kiwi", "Mango",
+    "Fries", "Pizza", "Burger", "Donut", "Bagel", "Toast", "Bacon", "Pasta", "Sushi", "Salad",
+    "Shake", "Cider", "Soda", "Lemon", "Lime", "Grape", "Melon", "Water", "Coffee", "Latte",
+    "Mocha", "Caramel", "Cookie", "Candy", "Brown", "Green", "Black", "White", "Sunny", "Cloud",
+    "Rainy", "Storm", "River", "Ocean", "Beach", "Mountain", "Forest", "Meadow", "Valley", "Desert",
+    "Canyon", "Field", "Plain", "Ridge", "Slope", "Curve", "Trail", "Track", "Grove", "Orchard",
+    "Quarry", "Reef", "Shoal", "Canyon", "Peak", "Plate", "Crust", "Delta"
   ];
 
 export default fiveLetterWords;
